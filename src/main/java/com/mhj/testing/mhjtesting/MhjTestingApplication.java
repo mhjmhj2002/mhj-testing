a80860c8f571f7c039ef378f4cd11c1bce9fa06d
mhj <mhjmhj2002@gmail.com>
2022-04-10 12:10:33 -0300

    first commit

---
+++ src/main/java/com/mhj/testing/mhjtesting/MhjTestingApplication.java
@@ -0,0 +1,13 @@
+package com.mhj.testing.mhjtesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MhjTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MhjTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mhj/testing/mhjtesting/MhjTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhj.testing.mhjtesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MhjTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
